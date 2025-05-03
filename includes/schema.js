da94516a053be8a75a14e2aa568d41100a83fce8
Tyler Clifton <tclifton2017@gmail.com>
2025-05-03 19:01:13 +0000

    created js variable holding schema info

---
+++ includes/schema.js
@@ -0,0 +1,30 @@
+const schema_mbta = {
+    'alerts': {
+        'data_set': 'mbta.',
+        'tables': {
+            'external': 'external_mbta_alerts',
+            'bronze': 'bronze_mbta_alerts_historical',
+            's1': 's1_mbta_alerts'
+        },
+        'fields': {
+            'dates': [
+                'alert_start',
+                'alert_end',
+            ],
+            'dimensions': [
+                'routes',
+                'id',
+                'header',
+                'description',
+                'cause',
+                'effect',
+                'severity',
+                'lifecycle'
+            ],
+            'etl_fields': [
+                'ingestion_datetime',
+                'ingestion_source'
+            ]
+        }
+    }
+}

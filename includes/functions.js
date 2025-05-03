85acba208d90d4bcb38c332a593b3d256d8df4ce
Tyler Clifton <tclifton2017@gmail.com>
2025-05-03 21:24:27 +0000

    fixed schema reference issues

--- includes/functions.js
+++ includes/functions.js
@@ -2,7 +2,7 @@
 const schemas = require('./schemas');
 
 // data sets
-const data_set = schemas.data_set;
+const data_set = schemas.alerts.data_set;
 
 // tables
 const alerts_tables_external = schemas.alerts.tables.external;
@@ -11,16 +11,17 @@ const alerts_tables_s1 = schemas.alerts.tables.s1;
 const alerts_tables_gold = schemas.alerts.tables.gold;
 
 // keys
-const alerts_keys = schemas.alerts.fields.keys;
+const alerts_key = schemas.alerts.fields.key;
 
 // dates
 const alerts_dates = schemas.alerts.fields.dates;
 
 // dimensions
-const alerts_dimensions_bronze = schemas.alerts.fields.dimensions.bronze;
+const alerts_dimensions = schemas.alerts.fields.dimensions;
 const etl_fields = schemas.alerts.fields.etl_fields;
 
 function SelectStatement(
+    data_set,
     source_table,
     dates,
     dimensions,
@@ -32,10 +33,11 @@ function SelectStatement(
         ${dimensions},
         ${etl_fields}
     FROM ${data_set + source_table}
-    GROUP BY ALL`
+    GROUP BY ALL;`
 }
 
 function DeleteStatement(
+    data_set,
     destination_table,
     source_table,
     key_field
@@ -47,21 +49,23 @@ function DeleteStatement(
             SELECT DISTINCT
                 ${key_field}
             FROM ${data_set + source_table}
-        )`
+        );`
 }
 
 function BronzeDesert() {
 
     const delete_statement = DeleteStatement(
+        data_set,
         alerts_tables_bronze,
         alerts_tables_external,
-        alerts_keys
+        alerts_key
     )
 
     const select_statement = SelectStatement(
+        data_set,
         alerts_tables_external,
         alerts_dates,
-        alerts_dimensions_bronze,
+        alerts_dimensions,
         etl_fields
     )
 

a82d3c1c068d94e586ff2b61de5dd78a74205bd0
Tyler Clifton <tclifton2017@gmail.com>
2025-05-03 21:01:21 +0000

    create functions for bronze and s1 layers

--- includes/functions.js
+++ includes/functions.js
@@ -1,1 +1,76 @@
 // variable imports
+const schemas = require('./schemas');
+
+// data sets
+const data_set = schemas.data_set;
+
+// tables
+const alerts_tables_external = schemas.alerts.tables.external;
+const alerts_tables_bronze = schemas.alerts.tables.bronze;
+const alerts_tables_s1 = schemas.alerts.tables.s1;
+const alerts_tables_gold = schemas.alerts.tables.gold;
+
+// keys
+const alerts_keys = schemas.alerts.fields.keys;
+
+// dates
+const alerts_dates = schemas.alerts.fields.dates;
+
+// dimensions
+const alerts_dimensions_bronze = schemas.alerts.fields.dimensions.bronze;
+const etl_fields = schemas.alerts.fields.etl_fields;
+
+function SelectStatement(
+    source_table,
+    dates,
+    dimensions,
+    etl_fields
+) {
+    return `
+    SELECT
+        ${dates},
+        ${dimensions},
+        ${etl_fields}
+    FROM ${data_set + source_table}
+    GROUP BY ALL`
+}
+
+function DeleteStatement(
+    destination_table,
+    source_table,
+    key_field
+) {
+    return `
+    DELETE FROM ${data_set + destination_table}
+    WHERE
+        ${key_field} IN (
+            SELECT DISTINCT
+                ${key_field}
+            FROM ${data_set + source_table}
+        )`
+}
+
+function BronzeDesert() {
+
+    const delete_statement = DeleteStatement(
+        alerts_tables_bronze,
+        alerts_tables_external,
+        alerts_keys
+    )
+
+    const select_statement = SelectStatement(
+        alerts_tables_external,
+        alerts_dates,
+        alerts_dimensions_bronze,
+        etl_fields
+    )
+
+    return {
+        delete_statement,
+        select_statement
+    }
+}
+
+module.exports = {
+    BronzeDesert
+}
--- includes/schemas.js
+++ includes/schemas.js
@@ -1,30 +1,33 @@
-const schema = {
-    'alerts': {
-        'data_set': 'mbta.',
-        'tables': {
-            'external': 'external_mbta_alerts',
-            'bronze': 'bronze_mbta_alerts_historical',
-            's1': 's1_mbta_alerts'
-        },
-        'fields': {
-            'dates': [
-                'alert_start',
-                'alert_end',
-            ],
-            'dimensions': [
-                'routes',
-                'id',
-                'header',
-                'description',
-                'cause',
-                'effect',
-                'severity',
-                'lifecycle'
-            ],
-            'etl_fields': [
-                'ingestion_datetime',
-                'ingestion_source'
-            ]
-        }
+const alerts = {
+    'data_set': 'mbta.',
+    'tables': {
+        'external': 'external_mbta_alerts',
+        'bronze': 'bronze_mbta_alerts_historical',
+        's1': 's1_mbta_alerts'
+    },
+    'fields': {
+        'keys': 'id',
+        'dates': [
+            'alert_start',
+            'alert_end'
+        ],
+        'dimensions': [
+            'routes',
+            'id',
+            'header',
+            'description',
+            'cause',
+            'effect',
+            'severity',
+            'lifecycle'
+        ],
+        'etl_fields': [
+            'ingestion_datetime',
+            'ingestion_source'
+        ]
     }
+}
+
+module.exports = {
+    alerts
 }

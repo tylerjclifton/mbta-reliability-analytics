95e8901ae2a9edb8f3e6b2935e6126c255ab781e
Tyler Clifton <tclifton2017@gmail.com>
2025-07-18 01:33:41 +0000

    updated schema map to be one const variable for less arguments needed in desert function

--- includes/schemas.js
+++ includes/schemas.js
@@ -1,60 +1,60 @@
-const alerts = {
-    'dataSet': 'mbta',
-    'tables': {
-        'external': 'external_alerts',
-        'bronze': 'bronze_alerts',
-        'gold': 'gold_alerts'
+const schemaMap = {
+    'alerts': {
+        'dataSet': 'mbta',
+        'tables': {
+            'external': 'external_alerts',
+            'bronze': 'bronze_alerts',
+            'gold': 'gold_alerts'
+        },
+        'fields': {
+            'key': 'id',
+            'dimensions': [
+                'alert_start',
+                'alert_end',
+                'id',
+                'route',
+                'header',
+                'description',
+                'cause',
+                'effect',
+                'severity',
+                'lifecycle'
+            ],
+            'ingestionInfo': [
+                'ingestion_datetime',
+                'ingestion_source'
+            ]
+        }
     },
-    'fields': {
-        'key': 'id',
-        'dimensions': [
-            'alert_start',
-            'alert_end',
-            'id',
-            'route',
-            'header',
-            'description',
-            'cause',
-            'effect',
-            'severity',
-            'lifecycle'
-        ],
-        'ingestionInfo': [
-            'ingestion_datetime',
-            'ingestion_source'
-        ]
-    }
-}
-
-const routes = {
-    'dataSet': 'mbta',
-    'tables': {
-        'external': 'external_routes',
-        'bronze': 'bronze_routes',
-        'gold': 'gold_routes'
-    },
-    'fields': {
-        'key': 'id',
-        'dimensions': [
-            'id',
-            'color',
-            'description',
-            'direction_destination_1',
-            'direction_destination_2',
-            'direction_name_1',
-            'direction_name_2',
-            'fare_class',
-            'long_name',
-            'links',
-        ],
-        'ingestionInfo': [
-            'ingestion_datetime',
-            'ingestion_source'
-        ]
+    'routes': {
+        'dataSet': 'mbta',
+        'tables': {
+            'external': 'external_routes',
+            'bronze': 'bronze_routes',
+            'gold': 'gold_routes'
+        },
+        'fields': {
+            'key': 'id',
+            'dimensions': [
+                'id',
+                'color',
+                'description',
+                'direction_destination_1',
+                'direction_destination_2',
+                'direction_name_1',
+                'direction_name_2',
+                'fare_class',
+                'long_name',
+                'links',
+            ],
+            'ingestionInfo': [
+                'ingestion_datetime',
+                'ingestion_source'
+            ]
+        }
     }
 }
 
 module.exports = {
-    alerts,
-    routes
+    schemaMap
 }

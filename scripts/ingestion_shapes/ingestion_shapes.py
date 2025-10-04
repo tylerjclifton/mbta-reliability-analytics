83f8ff69186b432ee113ce12f1f435467e6e0859
Tyler Clifton <tylerjclifton@outlook.com>
2025-10-04 11:17:42 -0400

    added extra line for formatting

--- scripts/ingestion_shapes/ingestion_shapes.py
+++ scripts/ingestion_shapes/ingestion_shapes.py
@@ -17,6 +17,7 @@ try:
     # Filter to Red, Blue, Orange, and Green routes
     # Set request to timeout after 30 seconds
     response = requests.get('https://api-v3.mbta.com/shapes?filter[route]=Red,Blue,Orange,Green-B,Green-C,Green-D,Green-E', timeout=30)
+    
 # If request times out
 except requests.exceptions.Timeout:
     # Log that the request timed out

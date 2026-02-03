b9f7feb8879ba9ce2670d12210c899a1f142d3c4
Tyler Clifton <tylerjclifton@outlook.com>
2026-02-03 18:38:59 -0500

    added info for cloud run jobs to file

---
+++ terraform/cloud-run-jobs.tf
@@ -0,0 +1,43 @@
+resource "google_cloud_run_v2_job" "ingestion_alerts" {
+  name     = "ingestion-alerts"
+  location = var.location
+  template {
+    task_count  = 1       # Total number of tasks to run
+    parallelism = 1        # Maximum number of tasks to run concurrently
+    template {
+      containers {
+        image = "us-east1-docker.pkg.dev/mbta-reliability-analytics/data-ingestion/ingestion-alerts@sha256:c50e7cea84f2e6831ff6916f8fe7744e273d7497cf26404f3457ebf8b06b7f19"
+        resources {
+            limits = {
+                memory = "512Mi"
+                cpu    = "1"
+                timeout     = "5m"
+                max_retries = 3
+            }
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_v2_job" "ingestion_routes" {
+  name     = "ingestion-routes"
+  location = var.location
+  template {
+    task_count  = 1       # Total number of tasks to run
+    parallelism = 1        # Maximum number of tasks to run concurrently
+    template {
+      containers {
+        image = "us-east1-docker.pkg.dev/mbta-reliability-analytics/data-ingestion/ingestion-routes@sha256:d89fee1b441b1bc5b7d94788f3ff176db88f5f248bbec23e92ed65c5979a3d61"
+        resources {
+            limits = {
+                memory = "512Mi"
+                cpu    = "1"
+                timeout     = "5m"
+                max_retries = 3
+            }
+        }
+      }
+    }
+  }
+}

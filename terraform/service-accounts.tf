db9016a423ba24d8a90b19be60aa315279ae55e8
Tyler Clifton <tylerjclifton@outlook.com>
2026-02-03 17:33:39 -0500

    renamed file to be plural and updated sa name to be standardized

--- terraform/service-accounts.tf
+++ terraform/service-accounts.tf
@@ -1,5 +1,5 @@
 # Terraform Runner Service Account
-resource "google_service_account" "terraform_runner_sa" {
+resource "google_service_account" "custom_sa_terraform_runner" {
   account_id   = "terraform-runner"
   display_name = "Terraform Runner"
 }

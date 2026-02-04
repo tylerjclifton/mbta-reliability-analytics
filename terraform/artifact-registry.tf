bad0aeb379db497818e91eddd66c9fd16648167d
Tyler Clifton <tylerjclifton@outlook.com>
2026-02-03 19:00:52 -0500

    removed iam resource and will add that in iam file

--- terraform/artifact-registry.tf
+++ terraform/artifact-registry.tf
@@ -6,10 +6,3 @@ resource "google_artifact_registry_repository" "data-ingestion" {
   format       = "DOCKER"
   description  = "Repository for storing Docker images for Cloud Run Jobs"
 }
-
-# IAM Role for Artifact Registry Access
-resource "google_artifact_registry_repository_iam_member" "artifact_registry_access" {
-  repository = google_artifact_registry_repository.data-ingestion.id
-  role       = "roles/artifactregistry.admin"
-  member     = "serviceAccount:${google_service_account.cloud_run_sa.email}"  # Replace with your service account
-}

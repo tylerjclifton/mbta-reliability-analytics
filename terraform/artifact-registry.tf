6a419205d77509e8dc77488bbd0c0ba685ea56db
Tyler Clifton <tylerjclifton@outlook.com>
2026-02-09 18:26:08 -0500

    removed name of repo will use id field

--- terraform/artifact-registry.tf
+++ terraform/artifact-registry.tf
@@ -1,6 +1,5 @@
 # Artifact Registry for Docker Containers
 resource "google_artifact_registry_repository" "data-ingestion" {
-  name         = "data-ingestion"
   location     = var.location
   repository_id = "data-ingestion"
   format       = "DOCKER"

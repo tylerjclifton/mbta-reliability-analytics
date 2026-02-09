1ec7c71da8bf923e572d87701b8d2e76d13436aa
Tyler Clifton <tylerjclifton@outlook.com>
2026-02-09 17:57:57 -0500

    adding new service account for dbt

--- terraform/service-accounts.tf
+++ terraform/service-accounts.tf
@@ -3,3 +3,9 @@ resource "google_service_account" "terraform_runner" {
   account_id   = "terraform-runner"
   display_name = "Terraform Runner"
 }
+
+# dbt Service Account
+resource "google_service_account" "dbt_bigquery" {
+  account_id   = "dbt-bigquery"
+  display_name = "dbt BigQuery"
+}
